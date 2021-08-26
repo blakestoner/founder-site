40c552c7ab0f96a2bb5f519df419f8b451a8785b
Blake Stoner <stonerblake@gmail.com>
2021-08-26 14:08:52 -0400

    2nd push

---
+++ src/components/SocialList.jsx
@@ -0,0 +1,13 @@
+import { Row, Col } from "react-bootstrap";
+
+export function SocialList() {
+  return (
+    <Row>
+      <Col></Col>
+      <Col></Col>
+      <Col></Col>
+      <Col></Col>
+      <Col></Col>
+    </Row>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,22 @@
+import { Container, Row, Col } from "react-bootstrap";
 import Layout from "../components/Layout";
+import { SocialList } from "../components/SocialList";
 
 export default function Home() {
-  return <Layout>Blah blah content here</Layout>;
+  return (
+    <Layout>
+      <Container>
+        <Row
+          style={{ display: "flex", alignItems: "center" }}
+        >
+          <Col>
+            <p className="lead">Founder of Vngle</p>
+            <h1 className="display-1">Blake Stoner</h1>
+            <SocialList />
+          </Col>
+          <Col></Col>
+        </Row>
+      </Container>
+    </Layout>
+  );
 }
