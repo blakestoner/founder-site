284f9fbfbf8bdb8dddc31a3ec5121e8aa6ef8e52
Blake Stoner <stonerblake@gmail.com>
2021-08-31 03:10:18 -0400

    fix bootstrap issue

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Home from './pages/Home';
 import About from './pages/About';
