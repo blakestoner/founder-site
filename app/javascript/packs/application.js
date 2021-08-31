47bdd4bff0725c8f410c6d423598bd75e688a01c
Blake Stoner <stonerblake@gmail.com>
2021-08-31 03:32:26 -0400

    fix CSS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@ import Rails from '@rails/ujs';
 import Turbolinks from 'turbolinks';
 import * as ActiveStorage from '@rails/activestorage';
 import 'bootstrap';
-import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 Rails.start();
 Turbolinks.start();
